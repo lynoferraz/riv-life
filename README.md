14e256640db8541cb43aa48d8fffdaa893974e39
Lyno Ferraz <lynoFerraz@gmail.com>
2024-05-22 14:18:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Life is Hard - Riv Cartridge
+
+A Conway's Game of Life-based puzzle, that you set up life and watch it evolve
+
+## Description
+
+How many and for how long life can survive? Set up Conway's Game of Life patterns and let it roll!
+
+The Life Points (LP) are spent to keep cells alive. But too few and stale cells are boring, so they consume more LP. Leveling up increase score income, but it also consumes LP faster. Birth award bonus, while deaths... And those pesky blue cells? They are just part of life, you'll have to live with them.
+
+Game controls:
+
+- Press ARROWS to move the cursor and keep it pressed to move faster.
+- Press Z/A1 to set/unset cell.
+- Keep X/A2 pressed to start simulation.
+
+## Game arguments
+
+| Argument | Type | Default Value | Description |
+| - | - | - | - |
+| -life-points | int | 10000 | Starting LP, to be consumed |
+| -birth-bonus | int | 1 | Score bonus for each new cell |
+| -starting-alive-bonus | int | 0 | Score bonus for each one of the remaining starting cells |
+| -death-penalty | int | 0 | Score penalty for each cell that dies |
+| -stale-penalty | int | 3 | Score penalty for each new cell that stayed alive |
+| -boring-threshold | int | 20 | Minimum number of cells to avoid boring penalty |
+| -boring-penalty | int | 100 | Penalty for having less than boring-threshold |
+| -starting-cells | int | 30 | Number of starting unremovable cells |
+| -level-increase | float | 500 | Score/level to reach to upgrade level |
+| -level-increase-factor | float | 2 | Factor to multiply level-increase after upgrading level |
+| efficiency | float | 1 | Rate of LP consumed by alive cells (rounded up) |
+| -setup-time | int | 30 | Time to set up life board |
+| -show-stats | bool | 1 | Display/Hide stats during simulation |
---
+++ life.c
@@ -0,0 +1,567 @@
+#include <riv.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool setingup;
+bool setup;
+bool starting;
+bool started;
+bool ended;
+
+enum {
+  SCREEN_SIZE = 256,
+  MAP_SIZE = 64,
+  TILE_SIZE = 4,
+  DIR_KEY_FRAMES = 4,
+  KEY_SET_FRAMES = 5,
+  A2_SET_FRAMES_START = 240,
+  N_LEVELS_SYSTEM = 12,
+  LEVEL_UP_ANIMATION_FRAMES = 6,
+  MIN_FRAME_SOUND_LEVEL = 6,
+  SPEED1 = 2,
+  SPEED2 = 8,
+  STARTING_FRAMES = 30,
+  SETINGUP_FRAMES = 90,
+};
+
+static uint32_t level_colors[] = {19,18,17,16,15,27,28,29,23,24,25,26};
+
+static int map[MAP_SIZE][MAP_SIZE];
+static int next_map[MAP_SIZE][MAP_SIZE];
+static riv_vec2i cursor_pos = {(SCREEN_SIZE-TILE_SIZE)/2, (SCREEN_SIZE-TILE_SIZE)/2};
+static int64_t cursor_speed = SPEED1;
+riv_vec2i cursor_map_pos;
+
+int u_press = 0;
+int d_press = 0;
+int l_press = 0;
+int r_press = 0;
+int a1_press = 0;
+int a2_press = 0;
+uint64_t a2_press_frame;
+int level_up_animation = 0;
+
+uint64_t mark_frame;
+uint64_t last_sound_frame;
+int level = 1;
+int deaths = 0;
+int births = 0;
+int current_alive = 0;
+int starting_alive = 0;
+int score = 0;
+
+int life_points = 10000;
+float level_increase = 500;
+float level_increase_factor = 2;
+int birth_bonus = 1;
+int starting_alive_bonus = 0;
+int death_penalty = 0;
+int stale_penalty = 3;
+int boring_penalty = 100;
+int boring_threshold = 20;
+int starting_cells = 30;
+int setup_time = 30;
+bool show_stats = 1;
+float efficiency = 1.0;
+
+
+int64_t clamp(int64_t v, int64_t min, int64_t max) { v = v < min ? min : v; v = v > max ? max : v; return v; }
+
+////
+// Sound
+
+// Sound effects
+riv_waveform_desc start_sfx = {
+    .type = RIV_WAVEFORM_PULSE,
+    .attack = 0.01f, .decay = 0.01f, .sustain = 0.25f, .release = 0.25f,
+    .start_frequency = RIV_NOTE_A3, .end_frequency = RIV_NOTE_A4,
+    .amplitude = 0.25f, .sustain_level = 0.5f,
+};
+riv_waveform_desc end_sfx = {
+    .type = RIV_WAVEFORM_PULSE,
+    .attack = 0.01f, .decay = 0.01f, .sustain = 0.1f, .release = 0.01f,
+    .start_frequency = RIV_NOTE_A3, .end_frequency = RIV_NOTE_A2,
+    .amplitude = 0.5f, .sustain_level = 0.5f,
+};
+riv_waveform_desc set_sfx = {
+    .type = RIV_WAVEFORM_PULSE,
+    .attack = 0.01f, .decay = 0.01f, .sustain = 0.1f, .release = 0.01f,
+    .start_frequency = RIV_NOTE_A4, .end_frequency = RIV_NOTE_C4,
+    .amplitude = 0.25f, .sustain_level = 0.5f,
+};
+riv_waveform_desc unset_sfx = {
+    .type = RIV_WAVEFORM_PULSE,
+    .attack = 0.01f, .decay = 0.01f, .sustain = 0.1f, .release = 0.01f,
+    .start_frequency = RIV_NOTE_C4, .end_frequency = RIV_NOTE_A4,
+    .amplitude = 0.25f, .sustain_level = 0.5f,
+};
+
+riv_waveform_desc ready_sfx = {
+    .type = RIV_WAVEFORM_PULSE,
+    .attack = 0.00f, .decay = 0.25f, .sustain = 0.0f, .release = 0.25f,
+    .start_frequency = RIV_NOTE_F4, .end_frequency = RIV_NOTE_F4,
+    .amplitude = 0.25f, .sustain_level = 0.25f,
+};
+
+riv_waveform_desc level_sfx = {
+    .type = RIV_WAVEFORM_SAWTOOTH,
+    .attack = 0.00f, .decay = 0.25f, .sustain = 0.25f, .release = 0.25f,
+    .start_frequency = RIV_NOTE_F4, .end_frequency = RIV_NOTE_G5,
+    .amplitude = 0.25f, .sustain_level = 0.25f,
+};
+
+riv_waveform_desc update_sfx = {
+    .type = RIV_WAVEFORM_SAWTOOTH,
+    .attack = 0.075f, .decay = 0.075f, .sustain = 0.1f, .release = 0.025f,
+    .start_frequency = RIV_NOTE_G2, .end_frequency = RIV_NOTE_A2,
+    .amplitude = 0.125f, .sustain_level = 0.1f,
+};
+
+
+static float rel_note_level[] = {
+    (261.63/261.63),
+    (293.66/261.63),
+    (329.63/261.63),
+    (349.23/261.63),
+    (392.00/261.63),
+    (440.00/261.63),
+    (493.88/261.63),
+    (523.25/261.63),
+    (587.33/261.63),
+    (659.25/261.63),
+    (698.46/261.63),
+    (783.99/261.63),
+    (880.00/261.63),
+};
+
+////
+// Main life logic
+
+int adjacencies (int i, int j) {
+	int	k, l, count;
+
+	count = 0;
+
+	/* go around the cell */
+	for (k=-1; k<=1; k++) for (l=-1; l<=1; l++){
+		/* only count if at least one of k,l isn't zero */
+        int c = i-k;
+        int r = j-l;
+		if (c >= 0 && r >= 0 && c < MAP_SIZE && r < MAP_SIZE)
+			if (k || l)
+                if (map[c][r]) count++;
+    }
+	return count;
+}
+
+
+////
+// Update functions
+
+void start_setup() {
+    cursor_map_pos = (riv_vec2i){(cursor_pos.x + TILE_SIZE/2) / TILE_SIZE, (cursor_pos.y + TILE_SIZE/2) / TILE_SIZE};
+    setingup = true;
+    riv_waveform(&start_sfx);
+
+    int count = 0;
+    while (count < starting_cells) {
+        riv_vec2i new_cell = (riv_vec2i){riv_rand_uint(MAP_SIZE-1), riv_rand_uint(MAP_SIZE-1)};
+        if (map[new_cell.x][new_cell.y] == 0) {
+            map[new_cell.x][new_cell.y] = 2;
+            count++;
+        }
+    }
+}
+
+void start_game() {
+    current_alive = 0;
+    starting_alive = 0;
+	for (int i=0; i<MAP_SIZE; i++) for (int j=0; j<MAP_SIZE; j++) {
+        if (map[i][j]) current_alive++;
+        if (map[i][j] == 2) starting_alive++;
+    }
+    life_points -= current_alive;
+    riv_waveform_desc start_waveform = *(&ready_sfx);
+    start_waveform.sustain = 0.25f;
+    riv_waveform(&start_waveform);
+    starting = true;
+    mark_frame = riv->frame;
+}
+
+void game_setingup() {
+    if (riv->frame - mark_frame > SETINGUP_FRAMES) {
+        mark_frame = riv->frame;
+        setup = true;
+    }
+}
+
+void game_starting() {
+    if (riv->frame - mark_frame > STARTING_FRAMES) {
+        mark_frame = riv->frame;
+        started = true;
+    }
+}
+
+// Called when game ends
+void end_game() {
+    // Play end sound
+    // riv_waveform(&end_sfx);
+    ended = true;
+    riv_waveform(&end_sfx);
+    // Quit in 3 seconds
+    riv->quit_frame = riv->frame + 2*riv->target_fps;
+}
+
+void update_setup() {
+
+    if (!(r_press || l_press || d_press || u_press)){
+        cursor_speed = SPEED1;
+    }
+    
+    if (riv->keys[RIV_GAMEPAD_RIGHT].down) r_press++; else r_press = 0;
+    if (r_press%DIR_KEY_FRAMES == 1) {
+        cursor_pos.x += cursor_speed;
+    } else if (r_press == 4*DIR_KEY_FRAMES) {
+        cursor_speed = SPEED2;
+    }
+
+    if (riv->keys[RIV_GAMEPAD_LEFT].down) l_press++; else l_press = 0;
+    if (l_press%DIR_KEY_FRAMES == 1) {
+        cursor_pos.x -= cursor_speed;
+    } else if (l_press == 4*DIR_KEY_FRAMES) {
+        cursor_speed = SPEED2;
+    }
+
+    if (riv->keys[RIV_GAMEPAD_DOWN].down) d_press++; else d_press = 0;
+    if (d_press%DIR_KEY_FRAMES == 1) {
+        cursor_pos.y += cursor_speed;
+    } else if (d_press == 4*DIR_KEY_FRAMES) {
+        cursor_speed = SPEED2;
+    }
+
+    if (riv->keys[RIV_GAMEPAD_UP].down) u_press++; else u_press = 0;
+    if (u_press%DIR_KEY_FRAMES == 1) {
+        cursor_pos.y -= cursor_speed;
+    } else if (u_press == 4*DIR_KEY_FRAMES) {
+        cursor_speed = SPEED2;
+    }
+
+
+    cursor_pos.x = clamp(cursor_pos.x, 0, SCREEN_SIZE-TILE_SIZE);
+    cursor_pos.y = clamp(cursor_pos.y, 0, SCREEN_SIZE-TILE_SIZE);
+
+    cursor_map_pos = (riv_vec2i){(cursor_pos.x + TILE_SIZE/2) / TILE_SIZE, (cursor_pos.y + TILE_SIZE/2) / TILE_SIZE};
+
+    if (riv->keys[RIV_GAMEPAD_A1].down) a1_press++; else a1_press = 0;
+    if (riv->keys[RIV_GAMEPAD_A2].down || (riv->frame - mark_frame)/riv->target_fps > setup_time ) a2_press++; else a2_press = 0;
+
+    if (a1_press == 1) {
+        if (map[cursor_map_pos.x][cursor_map_pos.y] == 2) { // can't change starting
+            riv_waveform(&end_sfx);
+        } else {
+            if (map[cursor_map_pos.x][cursor_map_pos.y]) riv_waveform(&set_sfx);
+            else riv_waveform(&unset_sfx);
+            map[cursor_map_pos.x][cursor_map_pos.y] = !map[cursor_map_pos.x][cursor_map_pos.y];
+        }
+    } else if (d_press == KEY_SET_FRAMES) a1_press = 0;
+    if (a2_press == 1) {
+        a2_press_frame = riv->frame;
+    } else if (a2_press == riv->target_fps || a2_press == 2*riv->target_fps || a2_press == 3*riv->target_fps) {
+        riv_waveform(&ready_sfx);
+    } else if (a2_press >= A2_SET_FRAMES_START) {
+        start_game();
+    }
+}
+
+void update_game() {
+    int a;
+
+    uint64_t rel_frame = riv->frame - mark_frame;
+
+    bool update_state = ((rel_frame % riv->target_fps) % clamp(N_LEVELS_SYSTEM - level,1,N_LEVELS_SYSTEM) == 0);
+
+    if (update_state) {
+        for (int i=0; i<MAP_SIZE; i++) for (int j=0; j<MAP_SIZE; j++) {
+            a = adjacencies (i, j);
+            if (a == 2) next_map[i][j] = map[i][j];
+            if (a == 3) next_map[i][j] = map[i][j] ? map[i][j] : 1;
+            if (a < 2) next_map[i][j] = 0;
+            if (a > 3) next_map[i][j] = 0;
+
+            if (map[i][j] && next_map[i][j]) {
+                life_points -= stale_penalty;
+            }
+
+            if (map[i][j] && !next_map[i][j]) {
+                deaths++;
+                life_points -= death_penalty;
+            }
+            if (!map[i][j] && next_map[i][j]) {
+                births++;
+                life_points += birth_bonus;
+            }
+        }
+        current_alive = 0;
+        starting_alive = 0;
+        for (int i=0; i<MAP_SIZE; i++) for (int j=0; j<MAP_SIZE; j++) {
+            map[i][j] = next_map[i][j];
+            if (map[i][j]) current_alive++;
+            if (map[i][j] == 2) starting_alive++;
+        }
+        life_points -= ceil(current_alive * efficiency);
+
+        if (boring_threshold > 0) {
+            life_points -= current_alive < boring_threshold ? boring_penalty * level: 0;
+        }
+
+        riv_waveform_desc waveform = *(&update_sfx);
+        int note = clamp(level-1,0,N_LEVELS_SYSTEM-1);
+        waveform.start_frequency = waveform.start_frequency * rel_note_level[note];
+        waveform.end_frequency = waveform.end_frequency * rel_note_level[note];
+        riv_waveform(&waveform);
+    }
+
+
+    score += level * current_alive + starting_alive * starting_alive_bonus;
+
+    // riv_printf("level up %f,\n",(float)score/(float)level_increase);
+    // update level
+    if ((float)score/level_increase >= (float)level) {
+        riv_waveform_desc level_waveform = *(&level_sfx);
+        int level_note = clamp(level-1,0,N_LEVELS_SYSTEM-1);
+        level_waveform.start_frequency = level_waveform.start_frequency * rel_note_level[level_note];
+        level_waveform.end_frequency = level_waveform.end_frequency * rel_note_level[level_note];
+        if (riv->frame - last_sound_frame > MIN_FRAME_SOUND_LEVEL) {
+            riv_waveform(&level_waveform);
+            last_sound_frame = riv->frame;
+        }
+        level_increase *= level_increase_factor;
+        level++;
+    }
+
+    riv->outcard_len = riv_snprintf((char*)riv->outcard, RIV_SIZE_OUTCARD, 
+        "JSON{\"score\":%d,\"current_alive\":%d,\"births\":%d,\"deaths\":%d,\"level\":%d,\"frame\":%d,\"starting_alive\":%d}", 
+        score, current_alive, births, deaths, level, rel_frame, starting_alive);
+
+    if (current_alive == 0 || life_points < 1) {
+        end_game();
+    }
+}
+
+
+////
+// Draw functions
+
+// Draw setup phase
+void draw_setup() {
+    // Clear screen
+    riv_clear(RIV_COLOR_DARKSLATE);
+    
+    int total_cells = 0;
+    // draw cells
+    for (int y=0;y<MAP_SIZE;++y) { // for each row
+        for (int x=0;x<MAP_SIZE;++x) { // for each column
+            int cell = map[x][y];
+            if (cell == 1) { // draw cell
+                riv_draw_rect_fill(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE, RIV_COLOR_LIGHTPEACH);
+                total_cells++;
+            } else if (cell == 2) { // draw fixed cell
+                riv_draw_rect_fill(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE, RIV_COLOR_LIGHTBLUE);
+                total_cells++;
+            } 
+        }
+    }
+    // draw cursor
+    uint32_t col = (riv->frame % 15 > 7) ? RIV_COLOR_LIGHTGREY : RIV_COLOR_GREY;
+    riv_draw_rect_line(cursor_map_pos.x*TILE_SIZE, cursor_map_pos.y*TILE_SIZE, TILE_SIZE, TILE_SIZE, col);
+    // draw text
+    if (a2_press < riv->target_fps/4){
+        riv_draw_text("Setup Life", RIV_SPRITESHEET_FONT_5X7, RIV_CENTER, 128, 10, 1, RIV_COLOR_WHITE);
+
+        char buf[128];
+        int remaing_time = setup_time-(riv->frame-mark_frame)/riv->target_fps;
+        riv_snprintf(buf, sizeof(buf), "%d", remaing_time > 0 ? remaing_time : 0);
+        riv_draw_text(buf, RIV_SPRITESHEET_FONT_5X7, RIV_LEFT, 10, 240, 1, RIV_COLOR_WHITE);
+    } else if (a2_press < riv->target_fps) {
+    } else if (a2_press < 2*riv->target_fps) {
+        int64_t size = (2*riv->target_fps - a2_press) / 12 + 2;
+        riv_draw_text("3", RIV_SPRITESHEET_FONT_5X7, RIV_CENTER, 128, 128, size, RIV_COLOR_WHITE);
+    } else if (a2_press < 3*riv->target_fps) {
+        int64_t size = (3*riv->target_fps - a2_press) / 12 + 2;
+        riv_draw_text("2", RIV_SPRITESHEET_FONT_5X7, RIV_CENTER, 128, 128, size, RIV_COLOR_WHITE);
+    } else if (a2_press < 4*riv->target_fps) {
+        int64_t size = (4*riv->target_fps - a2_press) / 12 + 2;
+        riv_draw_text("1", RIV_SPRITESHEET_FONT_5X7, RIV_CENTER, 128, 128, size, RIV_COLOR_WHITE);
+    }
+
+    char buf[128];
+    riv_snprintf(buf, sizeof(buf), "LP: %d",life_points - total_cells);
+    riv_draw_text(buf, RIV_SPRITESHEET_FONT_5X7, RIV_RIGHT, 246, 240, 1, RIV_COLOR_WHITE);
+}
+
+// Draw the game map
+void draw_game() {
+    // Clear screen
+    riv_clear(RIV_COLOR_DARKSLATE);
+    for (int y=0;y<MAP_SIZE;++y) { // for each row
+        for (int x=0;x<MAP_SIZE;++x) { // for each column
+            int cell = map[x][y];
+            if (cell > 0) { // draw tile sprite
+                uint32_t col = cell == 2 ? RIV_COLOR_LIGHTBLUE : level_colors[clamp(level-1,0,N_LEVELS_SYSTEM-1)];
+                riv_draw_rect_fill(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE, col);
+            }
+        }
+    }
+
+    if (show_stats) {
+        char buf[128];
+        riv_snprintf(buf, sizeof(buf), "Score: %d",score);
+        riv_draw_text(buf, RIV_SPRITESHEET_FONT_5X7, RIV_LEFT, 10, 240, 1, RIV_COLOR_WHITE);
+
+        riv_snprintf(buf, sizeof(buf), "LP: %d",life_points < 0 ? 0 : life_points);
+        riv_draw_text(buf, RIV_SPRITESHEET_FONT_5X7, RIV_RIGHT, 246, 240, 1, RIV_COLOR_WHITE);
+
+        riv_snprintf(buf, sizeof(buf), "Level: %d",level);
+        riv_draw_text(buf, RIV_SPRITESHEET_FONT_5X7, RIV_LEFT, 10, 10, 1, RIV_COLOR_WHITE);
+    }
+}
+
+void draw_start_screen() {
+    // Clear screen
+    riv_clear(RIV_COLOR_DARKSLATE);
+    // Draw snake title
+    riv_draw_text("Life is Hard", RIV_SPRITESHEET_FONT_5X7, RIV_CENTER, 128, 128, 2, RIV_COLOR_DARKPINK);
+
+    // Make "press to start blink" by changing the color depending on the frame number
+    uint32_t col = (riv->frame % 15 > 7) ? RIV_COLOR_LIGHTGREY : RIV_COLOR_GREY;
+    // Draw press to start
+    riv_draw_text("PRESS TO START", RIV_SPRITESHEET_FONT_5X7, RIV_CENTER, 128, 128+32, 1, col);
+}
+
+void draw_starting_screen() {
+    // Clear screen
+    riv_clear(riv->frame%2 ? RIV_COLOR_DARKSLATE : RIV_COLOR_LIGHTSLATE);
+
+    int dx = riv_rand_int(-1,1);
+    int dy = riv_rand_int(-1,1);
+
+    // Draw snake title
+    riv_draw_text("Life is Hard", RIV_SPRITESHEET_FONT_5X7, RIV_CENTER, 128+dx, 128+dy, 2, riv->frame%2 ? RIV_COLOR_DARKPINK : RIV_COLOR_DARKPURPLE);
+}
+
+// Draw game over screen
+void draw_end_screen() {
+    // Clear screen
+    riv_clear(RIV_COLOR_DARKSLATE);
+    // Draw last game frame
+    draw_game();
+    // Draw GAME OVER
+    // riv_draw_text("GAME OVER", RIV_SPRITESHEET_FONT_5X7, RIV_CENTER, 64, 64, 2, RIV_COLOR_RED);
+}
+
+
+////
+// Main dDraw and update functions
+
+// Called every frame to update game state
+void update() {
+    if (!setingup) { // Game not started yet
+        // Let enter setup phase whenever a key has been pressed
+        if (riv->key_toggle_count > 0) {
+            start_setup();
+        }
+    } else if (!setup) { // Game not in setup yet
+        game_setingup();
+    } else if (!starting) { // Game in setup
+        update_setup();
+    } else if (!started) { // Game is starting
+        game_starting();
+    } else if (!ended) { // Game is progressing
+        update_game();
+    }
+}
+
+// Called every frame to draw the game
+void draw() {
+    // Draw different screens depending on the game state
+    if (!setingup) { // Game not in setup yet
+        draw_start_screen();
+    } else if (!setup) { // Game not in setup yet
+        draw_starting_screen();
+    } else if (!starting) { // Game not started yet
+        draw_setup();
+    } else if (!started) { // Game not started yet
+        draw_game();
+    } else if (!ended) { // Game is progressing
+        draw_game();
+    } else { // Game ended
+        draw_end_screen();
+    }
+}
+
+////
+// Main loop
+
+int main(int argc, char* argv[]) {
+
+    if (argc > 1) {
+        if (argc % 2 == 0) {
+            riv_printf("Wrong number of arguments\n");
+            return 1;
+        }
+        for (int i = 1; i < argc; i+=2) {
+            // riv_printf("args %s=%s\n", argv[i],argv[i+1]);
+            if (strcmp(argv[i], "-life-points") == 0) {
+                life_points = atoi(argv[i+1]);
+            } else if (strcmp(argv[i], "-level-increase") == 0) {
+                char *endstr;
+                level_increase = strtof(argv[i+1], &endstr);
+            } else if (strcmp(argv[i], "-level-increase-factor") == 0) {
+                char *endstr;
+                level_increase_factor = strtof(argv[i+1], &endstr);
+                // riv_printf("stopped '%s' scan at '%s'\n",argv[i+1], endstr);
+            } else if (strcmp(argv[i], "-birth-bonus") == 0) {
+                birth_bonus = atoi(argv[i+1]);
+            } else if (strcmp(argv[i], "-death-penalty") == 0) {
+                death_penalty = atoi(argv[i+1]);
+            } else if (strcmp(argv[i], "-boring-penalty") == 0) {
+                boring_penalty = atoi(argv[i+1]);
+            } else if (strcmp(argv[i], "-starting-cells") == 0) {
+                starting_cells = atoi(argv[i+1]);
+            } else if (strcmp(argv[i], "-setup-time") == 0) {
+                setup_time = atoi(argv[i+1]);
+            } else if (strcmp(argv[i], "-show-stats") == 0) {
+                show_stats = atoi(argv[i+1]);
+            } else if (strcmp(argv[i], "-starting-alive-bonus") == 0) {
+                starting_alive_bonus = atoi(argv[i+1]);
+            } else if (strcmp(argv[i], "-efficiency") == 0) {
+                char *endstr;
+                efficiency = strtof(argv[i+1], &endstr);
+            } else if (strcmp(argv[i], "-boring-threshold") == 0) {
+                boring_threshold = atoi(argv[i+1]);
+            } else if (strcmp(argv[i], "-stale-penalty") == 0) {
+                stale_penalty = atoi(argv[i+1]);
+            }
+        }
+        // riv_printf("-life-points %d\n",life_points);
+        // riv_printf("-level-increase %f\n",level_increase);
+        // riv_printf("-level-increase_factor %f\n",level_increase_factor);
+        // riv_printf("-birth-bonus %d\n",birth_bonus);
+        // riv_printf("-stale-penalty %d\n",stale_penalty);
+        // riv_printf("-boring-penalty %d\n",boring_penalty);
+        // riv_printf("-boring-penalty %d\n",boring_penalty);
+        // riv_printf("-boring-threshold %d\n",boring_threshold);
+        // riv_printf("-starting-cells %d\n",starting_cells);
+        // riv_printf("-setup-time %d\n",setup_time);
+        // riv_printf("-show-stats %d\n",show_stats);
+        // riv_printf("-starting-alive-bonus %d\n",starting_alive_bonus);
+        // riv_printf("-efficiency %f\n",efficiency);
+    }
+
+    do { // main loop
+        update();
+        draw();
+    } while(riv_present());
+    return 0;
+}
